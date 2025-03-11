80fb42c9905abe7ed719fac77c3eacf9eb90151f
Kumordzi Joshua Klenam <163309124+JKumordzi@users.noreply.github.com>
2025-03-11 17:01:57 +0000

    update

---
+++ trial.js
@@ -0,0 +1,23 @@
+const arr = [
+  {
+    name: "JKL",
+    supervisor: "Josh",
+  },
+  {
+    name: "Josh",
+    supervisor: "",
+  },
+  { name: "Daniel", supervisor: "Josh" },
+  { name: "JKL", supervisor: "Josh" },
+  { name: "Veronica", supervisor: "Daniel" },
+  { name: "Bismark", supervisor: "JKL" },
+  { name: "Daniel", supervisor: "Josh" },
+];
+
+{
+    name: 'rihc'
+    a:[
+        {}
+    ]
+}
+
